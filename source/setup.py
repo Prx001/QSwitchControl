b471bd7163ad9adaa8de31112cfd77fcccf2a217
Prx001 <67240789+Prx001@users.noreply.github.com>
2021-07-30 00:14:03 +0430

    Update setup.py

--- source/setup.py
+++ source/setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
 	name="QSwitchControl",
 	version="1.0.0",
-	description="A ready and beautiful toggle switch for Qt Python binding PyQt",
+	description="An easy-to-use and modern toggle switch for Qt Python binding PyQt",
 	long_description=README,
 	long_description_content_type="text/markdown",
 	url="https://github.com/Prx001/QSwitchControl",
@@ -20,7 +20,6 @@ setup(
 		"Programming Language :: Python :: 3.9",
 		"Programming Language :: Python :: Implementation :: CPython"
 	],
-	packages=["QSwitchControl"],
 	include_package_data=True,
 	install_requires=["PyQt5"]
 )
