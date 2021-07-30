aa09bb7bb6b829139a4049b8f74bed3d144bf53f
Prx001 <67240789+Prx001@users.noreply.github.com>
2021-07-30 15:58:15 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 	long_description=README,
 	long_description_content_type="text/markdown",
 	url="https://github.com/Prx001/QSwitchControl",
-	author="Prx001",
+	author="Parsa.py",
 	author_email="munichbayern2005@gmail.com",
 	license="MIT",
 	classifiers=[
